2608e92e9880a7a9b22f40a4933da02490079b27
halloween-jack <halloween.jack.dev@gmail.com>
2017-11-05 07:01:27 +0900

    Building the environment: SKTexture and filling a path

--- Project29/GameScene.swift
+++ Project29/GameScene.swift
@@ -9,12 +9,37 @@
 import SpriteKit
 import GameplayKit
 
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case building = 2
+    case player = 4
+}
+
 class GameScene: SKScene {
+    var buildings = [BuildingNode]()
 
     override func didMove(to view: SKView) {
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        
+        createBuildings()
     }
     
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
+        
+        while currentX < 1024 {
+            let size = CGSize(width: RandomInt(min: 2, max: 4) * 40, height: RandomInt(min: 300, max: 600))
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: UIColor.red, size: size)
+            building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            building.setup()
+            addChild(building)
+            
+            buildings.append(building)
+        }
     }
 }
---
+++ Project29Tests/BuilingNode.swift
@@ -0,0 +1,13 @@
+//
+//  BuilingNode.swift
+//  Project29
+//
+//  Created by slim on 2017/11/05.
+//  Copyright © 2017 halloween-jack. All rights reserved.
+//
+
+import UIKit
+
+class BuilingNode: SKSpriteNode {
+
+}
